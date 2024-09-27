309bbd39e54ff9abf7d204551000864117c722d8
Secu Guru <secu.starts.here@gmail.com>
2024-09-26 17:35:50 -0700

    - link : https://leetcode.com/problems/ransom-note/

--- lc_383/lc_383_my_01.py
+++ lc_383/lc_383_my_01.py
@@ -1,22 +1,21 @@
 def main(ransomNote, magazine):
-
     m_dic = {}
     r_dic = {}
 
     for ch in magazine:
-        if ch in m_dic.keys():
+        if ch in m_dic:
             m_dic[ch] += 1
         else:
             m_dic[ch] = 1
 
     for ch in ransomNote:
-        if ch in r_dic.keys():
+        if ch in r_dic:
             r_dic[ch] += 1
         else:
             r_dic[ch] = 1
     
-    for key in r_dic.keys():
-        if key not in m_dic.keys():
+    for key in r_dic:
+        if key not in m_dic:
             return False
         
         if r_dic[key] > m_dic[key]:
